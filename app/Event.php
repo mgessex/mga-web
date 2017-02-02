7a0549f0339641089e8926f3e1f4394704fd1c9d
mgessex <michael@essexappeal.ca>
2017-02-02 16:49:52 -0700

    Complete

--- app/Event.php
+++ app/Event.php
@@ -15,17 +15,35 @@ class Event extends Model
     	}
     }
 
-    public function calcOpenAge()
+    public function isOpenRecent()
     {
-    	if(!empty($this->open_date) && !empty($this->open_time)){
-    		$openDate = new \DateTime($this->open_date);
-    		$openDate->setTime($this->open_time);
-    		$now = new \DateTime('now');
-    		$days = $openDate->diff($now)->h;
-    		return $days;
-    	}
-    	else {
-    		return  new \DateInterval(0);
-    	}
+    	if($this->status == 'Open') {   
+            if(!empty($this->open_date) && !empty($this->open_time)) {
+                // $openDate = new \DateTime($this->open_date);
+                // $openDate->setTime($this->open_time);
+                // $now = new \DateTime('now');
+                // $days = $openDate->diff($now)->h;
+                // return $days;
+    
+                // Compare now and open DateTime + 1 day (24hrs)
+                $openDateTime = new \DateTime($this->open_date.'T'.$this->open_time);
+                //$openDateTime->setTime($this->open_time);
+                $openDateTime->add(new \DateInterval('P1D'));
+                $now = new \DateTime('now');
+                // if Event open datetime + 1 day > now then this is the sweet spot
+                if($openDateTime > $now) {
+                    return true;
+                }
+                else {
+                    return false;
+                }
+            }
+            else {
+                return false;
+            }
+        }
+        else {
+            return false;
+        }
     }
 }
